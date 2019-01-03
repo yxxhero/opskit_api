4eeb4d4379da79c89ab3afd7f252504e60a30930
yxxhero <yxxhero@example.com>
2019-01-03 22:54:23 +0800

    调整代码结构

---
+++ config.py
@@ -0,0 +1,19 @@
+import multiprocessing
+import os
+import gevent.monkey
+gevent.monkey.patch_all()
+
+
+debug = True
+timeout = 90
+proc_name='opskit_api'
+loglevel = 'debug'
+bind = "0.0.0.0:5000"
+pidfile = "log/gunicorn.pid"
+accesslog = "log/access.log"
+errorlog = "log/debug.log"
+# daemon = True
+
+workers = multiprocessing.cpu_count() * 2
+worker_class = 'gevent'
+x_forwarded_for_header = 'X-FORWARDED-FOR'
