3c6a8bc846d2037bb6ec5371a8dedc65a6a8d69c
yxxhero <yxxhero@example.com>
2019-01-15 21:12:53 +0800

    add note type

--- opskit_api/models.py
+++ opskit_api/models.py
@@ -1,7 +1,8 @@
 import datetime
+from sqlalchemy_utils.types.choice import ChoiceType
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate 
+from flask_migrate import Migrate
 from flask_redis import FlaskRedis
 
 app = Flask(__name__)
@@ -12,19 +13,29 @@ migrate = Migrate(app, db)
 
 # model
 
+
 class Note(db.Model):
     __tablename__ = 'note'
+
+    NOTE_TYPES = [
+        ('database', 'Database'),
+        ('web','Web'),
+        ('docker', 'Docker'),
+        ('security', 'Security')
+    ]
 
     id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(256))
     content = db.Column(db.Text)
+    note_type = db.Column(ChoiceType(NOTE_TYPES))
     userId = db.Column(db.Integer, db.ForeignKey('user.id'))
     create_time = db.Column(db.DateTime)
 
     def __init__(self, title, content):
         self.title = title
-        self.content = content 
+        self.content = content
         self.create_time = datetime.utcnow()
+
 
 class User(db.Model):
     __tablename__ = "user"
@@ -34,5 +45,3 @@ class User(db.Model):
     user_password = db.Column(db.String(100))
     user_email = db.Column(db.String(512))
     note = db.relationship('Note', backref='user', lazy=True)
-
-
